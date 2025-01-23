91cf95edf347a3c5a68bfb9b1407a5a253713767
Henry Zhang <henry.zhang@wisetechglobal.com>
2025-01-24 05:54:58 +0800

    fix: remove the workaround code

--- NetCoreConsoleApp/Program.cs
+++ NetCoreConsoleApp/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System.Reflection;
 
-AppDomain.CurrentDomain.AssemblyResolve += (_, args) =>
-{
-    var assemblyName = new AssemblyName(args.Name).Name;
-    var assemblyPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{assemblyName}.dll");
-    return File.Exists(assemblyPath) ? Assembly.LoadFrom(assemblyPath) : null;
-};
-
 Assembly.Load("NormalClrAssembly");
 Assembly.Load("Microsoft.Data.SqlClient.SNI");
